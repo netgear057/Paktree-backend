1a9225d6f4d20c12f012f835e29a6e1a1a40e9c7
Netgear057 <netgear057@gmail.com>
2025-08-11 16:43:46 +0500

    supabase

--- config/supabase.js
+++ config/supabase.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+const { createClient } = require('@supabase/supabase-js')
 
 const supabaseUrl = 'https://iryueqfyscbdtgutzqnq.supabase.co'
 const supabaseKey = process.env.SERVICE_KEY
- export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
--- utils/UploadSupabase.js
+++ utils/UploadSupabase.js
@@ -1,4 +1,4 @@
-const { supabase } = require("../config/supabase")
+const  supabase  = require("../config/supabase")
 
 const uploadImageToSupabase = async (buffer, fileName, bucket = 'manddi') => {
   const { data, error } = await supabase.storage
