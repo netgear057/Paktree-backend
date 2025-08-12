4529e76a2e474d757b5eb84daeb7859ddaae5392
Netgear057 <netgear057@gmail.com>
2025-08-12 23:06:09 +0500

    cors

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ startExpireFeaturedJob()
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/stripe', stripeRouter)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +27,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
+app.use('/stripe', stripeRouter)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter)
