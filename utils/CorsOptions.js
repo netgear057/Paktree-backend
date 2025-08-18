e2a44703949574d4ad748a961292fba08883da86
Netgear057 <netgear057@gmail.com>
2025-08-18 12:03:21 +0500

    corsdynamic

---
+++ utils/CorsOptions.js
@@ -0,0 +1,15 @@
+
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
+export const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // allow requests like Postman or mobile apps
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
