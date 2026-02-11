2ce70910da87a2dabc89ce3d9c60addcb5614f4e
Netgear057 <netgear057@gmail.com>
2026-02-11 13:58:24 +0500

    new 1

--- api/index.js
+++ api/index.js
@@ -3,10 +3,10 @@ const app = require('../app');
 const connectDB = require('../config/db');
 
 // Connect DB before handling request
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
+// app.use(async (req, res, next) => {
+//   await connectDB();
+//   next();
+// });
 
 app.get('/ping', (req, res) => {
   res.json({ message: "API working" });
